1f4022ce71b67d7f54af18d6032162b88f537f86
fabo <fabo.dev.email@gmail.com>
2025-01-06 13:27:59 +0100

    #2
    producer service

--- src/kafka/kafka.module.ts
+++ src/kafka/kafka.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ProducerService } from './producer.service';
 
-@Module({})
+@Module({
+  exports: [ProducerService],
+  providers: [ProducerService],
+})
 export class KafkaModule {}
